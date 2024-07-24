cf97e08a9267e281fe942f23a46916cfddc4a648
IsabelliTostes <isa_tostes@hotmail.com>
2024-07-24 17:29:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+1 – Escreva um algoritmo para somar dois números e multiplicar o
+resultado pelo primeiro número*/
+
+$nota1= 20;
+$nota2= 30;
+
+$soma= $nota1 + $nota2;
+
+$mult= ($soma*$nota1);
+
+echo "$mult <br />\n";
+
+/*2– Escreva um algoritmo que leia três números nas variáveis Val1, Val2
+e Val3, calcule sua média na variável media e exiba para o usuário o resultado*/
+
+// 1. ler e declarar variaveis
+$Val1= 45;
+$Val2= 55;
+$Val3= 75;
+
+// 2. calcular media
+$media= ($Val1+ $Val2+ $Val3)/3;
+
+// 3. exiba para o usuário o resultado
+echo "$media <br />\n";
+
+/*
+Exercicio 3 – Escreva um algoritmo que receba um valor,
+q calcule e 
+mostre para usuário 15% deste valor.*/ 
+
+ $a=12;
+ $calc= $a*0.15;
+
+ echo "$calc <br />\n";
+
+ /*
+ Exercicio 4 – Escreva um algoritmo que receba um valor, 
+ calcule e 
+ mostre para o usuário 5% 
+ e 50% deste valo.*/
+
+ $pudim=15;
+ $calda=20;
+ $gula= ($pudim + $calda)*5;
+
+ $gula2= ($gula)*50;
+
+ echo " $gula2 <br />\n";
